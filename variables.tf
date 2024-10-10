502334bac625f1758a8cebfd9e4fb5319351a2b2
datshit-sharma-neu <sharma.dar@northeastern.edu>
2024-10-10 03:58:48 -0400

    clean up

--- variables.tf
+++ variables.tf
@@ -13,3 +13,11 @@ variable "public_subnets_cidrs" {
 variable "private_subnets_cidrs" {
   type = list(string)
 }
+
+variable "aws_profile" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
