036dbfa1bf464a332fddd0ef83674706c7fa68e3
datshit-sharma-neu <sharma.dar@northeastern.edu>
2024-10-09 21:56:37 -0400

    Fix: terraform files formatted

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,11 +8,11 @@ resource "aws_vpc" "csye6225_vpc" {
 }
 
 # Public Subnets 
-resource "aws_subnet" "public_subnets"{
-  count = length(var.public_subnets_cidrs)
-  vpc_id = aws_vpc.csye6225_vpc.id
-  cidr_block = element(var.public_subnets_cidrs, count.index)
-  availability_zone = element(var.availability_zones, count.index)  
+resource "aws_subnet" "public_subnets" {
+  count             = length(var.public_subnets_cidrs)
+  vpc_id            = aws_vpc.csye6225_vpc.id
+  cidr_block        = element(var.public_subnets_cidrs, count.index)
+  availability_zone = element(var.availability_zones, count.index)
 
   tags = {
     Name = "csye6225_public_subnet_${count.index}"
@@ -20,11 +20,11 @@ resource "aws_subnet" "public_subnets"{
 }
 
 #Private Subnets
-resource "aws_subnet" "private_subnets"{
-  count = length(var.private_subnets_cidrs)
-  vpc_id = aws_vpc.csye6225_vpc.id
-  cidr_block = element(var.private_subnets_cidrs, count.index)
-  availability_zone = element(var.availability_zones, count.index)  
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnets_cidrs)
+  vpc_id            = aws_vpc.csye6225_vpc.id
+  cidr_block        = element(var.private_subnets_cidrs, count.index)
+  availability_zone = element(var.availability_zones, count.index)
 
   tags = {
     Name = "csye6225_public_subnet_${count.index}"
@@ -46,7 +46,7 @@ resource "aws_route_table" "public_route_table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.csye6225_igw.id  
+    gateway_id = aws_internet_gateway.csye6225_igw.id
   }
 
   tags = {
@@ -55,9 +55,9 @@ resource "aws_route_table" "public_route_table" {
 }
 
 resource "aws_route_table_association" "public_route_table_association" {
-  count = length(var.public_subnets_cidrs)
+  count          = length(var.public_subnets_cidrs)
   subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
-  route_table_id = aws_route_table.public_route_table.id 
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 # Private Route Table
@@ -70,7 +70,7 @@ resource "aws_route_table" "private_route_table" {
 }
 
 resource "aws_route_table_association" "private_route_table_association" {
-  count = length(var.private_subnets_cidrs)
+  count          = length(var.private_subnets_cidrs)
   subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
-  route_table_id = aws_route_table.private_route_table.id 
+  route_table_id = aws_route_table.private_route_table.id
 }
